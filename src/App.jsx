2161d351b71486300f9e53108631760a7c5d3386
cinecity <johnnyrobertjay@gmail.com>
2024-11-11 20:05:19 -0500

    complete assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import data from "./notifications";
+
+
+
+import "./App.css";
+
+function NotificationItem({ onClear, children, name }) {
+  return (
+    <div className="my-notif">
+      <h3>{name}</h3>
+      {children}
+      <button onClick={onClear}>Clear</button>
+    </div>
+  );
+}
+
+function Notifications({ notifications, onClearNotification }) {
+  
+  return (
+    <div className="list">
+      {notifications.map((notification) => (
+        <NotificationItem
+          key={notification.id}
+          
+          name={notification.name}
+
+          onClear={() => onClearNotification(notification.id)}
+        >
+          <h3>{notification.title}</h3>
+          <p>{notification.message}</p>
+        </NotificationItem>
+      ))}
+    </div>
+  );
+}
+
+function ClearButton({ onClearAll, isDisabled }) {
+  return (
+    <button onClick={onClearAll} disabled={isDisabled}>
+      Clear All
+    </button>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notifications, setNotifications] = useState(data);
+
+  const handleClearNotification = (id) => {
+    setNotifications((prevNotifications) =>
+      prevNotifications.filter((notification) => notification.id !== id)
+    );
+  };
+
+  const handleClearAllNotifications = () => {
+    setNotifications([]);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        Notifications: {notifications.length}
+      </h1>
+      <Notifications
+        notifications={notifications}
+        onClearNotification={handleClearNotification}
+      />
+      <ClearButton
+        onClearAll={handleClearAllNotifications}
+        isDisabled={notifications.length === 0}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
